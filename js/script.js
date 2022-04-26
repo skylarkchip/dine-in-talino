404e09ac7b10c639cfce1e3a42bfa4a6e56824c3
Mark Marasigan <kramlivingstone@gmail.com>
2022-04-26 14:17:56 +0800

    Fixed Scroll Functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const siteBody = document.querySelector("body");
 const menuToggle = document.querySelector(".filterToggle");
 const menuDrawer = document.querySelector(".menuDrawer");
+const scrollBtn = document.querySelector(".scrollToBtn");
+const mainDiv = document.querySelector("#main");
 
 function addClasses(first, second) {
   first.classList.add("is-active");
@@ -19,7 +21,13 @@ const closeNav = (e) => {
   removeClasses(menuToggle, menuDrawer);
 };
 
+const scrollDown = () => {
+  mainDiv.scrollIntoView();
+};
+
 menuToggle.addEventListener("click", toggleNav);
+
+scrollBtn.addEventListener("click", scrollDown);
 
 document.addEventListener("click", (e) => {
   if (!menuToggle.contains(e.target)) closeNav();
